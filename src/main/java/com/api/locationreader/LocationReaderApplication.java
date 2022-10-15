ee59df2499a8f98738a4647b55d813c2ddb8e4e6
Muhammad Zaid <zaid.mhd@outlook.com>
2022-10-15 15:25:25 +0500

    Initial commit.

---
+++ src/main/java/com/api/locationreader/LocationReaderApplication.java
@@ -0,0 +1,13 @@
+package com.api.locationreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/locationreader/LocationReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.locationreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
